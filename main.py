6f8658dffdc7285c5f02dea93ed1f8ba1b498e5d
John Muinde <66753112+john-muinde@users.noreply.github.com>
2024-03-21 18:20:55 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer)
+    text = db.Column(db.Text)
+    isActive = db.Column(db.Boolean, default=True)
+ 
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def main_page():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-app.run(debug=True)
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+        text = request.form['text']
+        item = Item(title=title, price=price, text=text)
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            return f'Ошибка {e}'
+    else:
+        return render_template('create.html')
+    
+@app.route('/buy/<int:id>')
+def buy_course(id):
+    return str(id)
+
+if __name__ == '__main__':
+    app.run(debug=True)
