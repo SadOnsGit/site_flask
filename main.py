688cbcbc83b9be825f3083ea540393c4dbd60601
John Muinde <66753112+john-muinde@users.noreply.github.com>
2024-03-21 17:44:29 +0300

    123

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return 'Hello'
+    return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-app.run()
+app.run(debug=True)
